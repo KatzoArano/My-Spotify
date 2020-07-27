b9cee86fd10c5c59eece6c5e77f1e88d46d2e812
oarano <oarano@macbook-pro-de-oarano-2.home>
2020-07-27 08:05:58 +0200

    add mongoose.Promise for delete message bash

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient } = require("mongodb");
 const app = require('./app');
 const port = process.env.port || 5000
 
+mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost:27018/spotify", { useNewUrlParser: true,useUnifiedTopology: true }, (err, res )=> {
     if(err){
