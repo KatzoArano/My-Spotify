4b068982b6323c01320421b3d6ceaf585615d991
oarano <oarano@macbook-pro-de-oarano-2.home>
2020-07-30 17:01:11 +0200

    Configure HTTP Headers

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.use('/api', user_routes);
 
 
 
-//Cors
+// Configure HTTP Headers
+// CORS
+app.use((req, res, next)=>{
+    // Allow access to all domains
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 module.exports = app;
