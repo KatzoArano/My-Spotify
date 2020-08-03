0b173fd896a8b32bbaaa638163940c71e5e1e8b3
oarano <oarano@MacBook-Pro-de-oarano-2.local>
2020-08-03 11:28:03 +0200

    Controller && route saveAlbum

--- controllers/album.js
+++ controllers/album.js
@@ -13,10 +13,29 @@ function testAlbum(req,res){
     res.status(200).send({message: "Controllador album Ok"})
 }
 
-function getAlbum(req,res){
+function saveAlbum(req, res){
+    var album = new Album();
+    var params = req.body;
+    album.title = params.title;
+    album.description = params.description;
+    album.year = params.year;
+    album.image = 'null';
+    album.artist = params.artist;
 
+    album.save((err, albumStored) => {
+        if(err){
+            res.status(500).send({message: 'Server error'});
+        }else{
+            if(!albumStored){
+                res.status(404).send({message: 'Album cannot be stored'});
+            }else{
+                res.status(200).send({album: albumStored});
+            }
+        }
+    });
 }
 
 module.exports = {
-    testAlbum
+    testAlbum,
+    saveAlbum
 }
--- routes/album.js
+++ routes/album.js
@@ -5,5 +5,6 @@ const md_auth = require('../middelwares/authenticated');
 const api = express.Router();
 
 api.get('/testAlbum',md_auth.validationAuth, albumCtrl.testAlbum);
+api.post('/album',md_auth.validationAuth, albumCtrl.saveAlbum);
 
 module.exports = api;
